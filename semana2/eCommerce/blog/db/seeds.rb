3ae64c806f1e25f4b04c37020eb3a3c1d30b1761
agarciadelarco <alfgda@gmail.com>
2015-04-17 14:00:36 +0200

    añadido ejercicio semana 2

---
+++ semana2/eCommerce/blog/db/seeds.rb
@@ -0,0 +1,3 @@
+Laptop.create( brand: "HP", model: "ENVY", processor: "Intel® Core™ i7", ram: 16, price: 1049.00 )
+Laptop.create( brand: "Apple", model: "MacBook", processor: "Intel® Core™ M", ram: 8, price: 1449.00 )
+Laptop.create( brand: "Asus", model: "Transformer Book Flip", processor: "Intel® Core™ i7", ram: 8, price: 1080.21 )
