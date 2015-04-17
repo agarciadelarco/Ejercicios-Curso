d76f20865266057e1ad63a53674bf7cf03555bbc
agarciadelarco <alfgda@gmail.com>
2015-04-17 17:46:50 +0200

    cambios ejercicio semana2

--- semana2/eCommerce/blog/db/schema.rb
+++ semana2/eCommerce/blog/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416155117) do
+ActiveRecord::Schema.define(version: 20150417123257) do
+
+  create_table "backpacks", force: :cascade do |t|
+    t.string   "color"
+    t.integer  "capacity"
+    t.integer  "pockets"
+    t.string   "waterproof"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "laptops", force: :cascade do |t|
     t.string   "brand"
--- semana2/eCommerce/blog/db/seeds.rb
+++ semana2/eCommerce/blog/db/seeds.rb
@@ -1,3 +1,7 @@
 Laptop.create( brand: "HP", model: "ENVY", processor: "Intel® Core™ i7", ram: 16, price: 1049.00 )
 Laptop.create( brand: "Apple", model: "MacBook", processor: "Intel® Core™ M", ram: 8, price: 1449.00 )
 Laptop.create( brand: "Asus", model: "Transformer Book Flip", processor: "Intel® Core™ i7", ram: 8, price: 1080.21 )
+
+Backpack.create( color: "blue", capacity: 30, pockets: 6, waterproof: "yes")
+Backpack.create( color: "black", capacity: 20, pockets: 4, waterproof: "yes")
+Backpack.create( color: "red", capacity: 10, pockets: 1, waterproof: "no")
